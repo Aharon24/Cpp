03946346e85f56a30ede2ea22d272f0b1d5b0397
Aharon Petrosyan <petrosyan-ahar@mail.ru>
2026-01-28 20:33:14 +0400

    some part

---
+++ ex01/PhoneBook.hpp
@@ -0,0 +1,14 @@
+#include "Contact.hpp"
+#include <iostream>
+#include <string>
+
+class PhoneBook
+{
+
+    private:
+        /* data */
+    public:
+        PhoneBook(/* args */);
+        ~PhoneBook();
+};
+
---
+++ ex01/main.cpp
@@ -0,0 +1,39 @@
+#include "PhoneBook.hpp"
+
+int ft_chesk(int i)
+{
+	if (i != 1)
+	{
+		std::cout << "Error wrong argument count\n";
+		return (2);
+	}
+	std::cout << "Welcome to My Awesome PhoneBook!" << std::endl;
+	return (0);
+}
+
+
+int main (int argc)
+{
+	std::string command;
+
+	if (ft_chesk(argc) == 1)
+		return (1);
+	while(1)
+	{
+		std::cout << "Enter a command (ADD, SEARCH, EXIT): ";
+		std::getline(std::cin, command);
+		if (command == "ADD")
+		{
+
+		}
+		else if (command == "EXIT")
+		{
+			std::cout << "Goodbye!\n";
+			return (1);
+		}
+		else if (command == "SEARCH")
+		{
+			
+		}
+	}
+}
