97c2352ddb1147d3884c33671645a4feebf60965
Aharon Petrosyan <petrosyan-ahar@mail.ru>
2026-02-07 20:27:18 +0400

    ADD

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -1,6 +1,4 @@
-#include <iostream>
 #include "Account.hpp"
-
 
 int Account::getNbAccounts( void )
 {
@@ -21,5 +19,41 @@ int Account::getNbWithdrawals( void )
 }
 void    Account::displayAccountsInfos( void )
 {
-	
+    _displayTimestamp();
+    std::cout << "accounts:" << getNbAccounts() << ";";
+    std::cout << "total:" << getTotalAmount() << ";";
+    std::cout << "deposits:" << getNbDeposits() << ";";
+    std::cout << "withdrawals:" << getNbWithdrawals() << std::endl;
+}
+
+
+void Account::_displayTimestamp( void )
+{
+	std::time_t now = std::time(NULL);
+	std::tm *ltm = std::localtime(&now);
+
+  	std::cout << "["
+  	            << (ltm->tm_year + 1900)
+  	            << std::setw(2) << std::setfill('0') << (ltm->tm_mon + 1)
+  	            << std::setw(2) << std::setfill('0') << ltm->tm_mday
+  	            << "_"
+  	            << std::setw(2) << std::setfill('0') << ltm->tm_hour
+  	            << std::setw(2) << std::setfill('0') << ltm->tm_min
+  	            << std::setw(2) << std::setfill('0') << ltm->tm_sec
+  	            << "] ";
+}
+
+Account::Account(int initial_deposit)
+{
+	_accountIndex = _nbAccounts;
+	_nbAccounts++;
+
+	_amount = initial_deposit;
+    _totalAmount += initial_deposit;
+	_nbDeposits = 0;
+    _nbWithdrawals = 0;
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex
+          << ";amount:" << _amount
+          << ";created" << std::endl;
 }
