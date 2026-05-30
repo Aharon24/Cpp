278454c9f333be1c51641f9907880d2af2a03b24
Aharon Petrosyan <petrosyan-ahar@mail.ru>
2026-05-30 19:02:21 +0400

    ADD

--- cpp07/ex02/Array.cpp
+++ cpp07/ex02/Array.cpp
@@ -17,5 +17,9 @@ Array<T>::Array(unsigned int n)
 template <typename T>
 Array<T>::Array(const Array& other)
 {
-    
+     _size = other._size;
+    _array = new T[_size];
+
+    for (unsigned int i = 0; i < _size; i++)
+        _array[i] = other._array[i];
 }
